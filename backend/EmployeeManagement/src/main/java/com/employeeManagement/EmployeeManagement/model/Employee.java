29e79a9e9558cc2c60a280d7f925dc76bdd5092e
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-07 09:47:10 +0530

    Employee Entity created

---
+++ backend/EmployeeManagement/src/main/java/com/employeeManagement/EmployeeManagement/model/Employee.java
@@ -0,0 +1,31 @@
+package com.employeeManagement.EmployeeManagement.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email_id")
+    private String emailId;
+
+    public Employee(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+}
