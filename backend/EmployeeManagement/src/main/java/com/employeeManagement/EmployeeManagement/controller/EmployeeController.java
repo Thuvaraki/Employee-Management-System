86b2d903007e77b5e8273291c29de32a07f4fb6d
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-07 12:08:16 +0530

    Update EmployeeController.java

--- backend/EmployeeManagement/src/main/java/com/employeeManagement/EmployeeManagement/controller/EmployeeController.java
+++ backend/EmployeeManagement/src/main/java/com/employeeManagement/EmployeeManagement/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.employeeManagement.EmployeeManagement.controller;
 import com.employeeManagement.EmployeeManagement.model.Employee;
 import com.employeeManagement.EmployeeManagement.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
