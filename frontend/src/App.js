1d8b811e7c50f3cfd2018e04e55bd867d6c84414
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-07 13:00:28 +0530

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ListEmployeeComponent />
+      <Routes>
+        <Route path="/" element={<ListEmployeeComponent />} />
+      </Routes>
       <Footer />
     </div>
   );
